119852e618ac1560ddd6f0554d8361ef7964809f
Valente Licona <isc.lcv@gmail.com>
2024-06-09 09:39:34 -0600

    [ADD] Initial content of the landing page is created with styles supported by tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
